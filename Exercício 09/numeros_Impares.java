62a73ad00a06095a62a7eed00c9e65bcb36b0f53
Giovani Gomes <giovanigomes2002@gmail.com>
2022-05-15 07:58:08 -0300

    Exercício 09
    Resolução do Exercício 09 Lista 03 Java e sua descrição.

---
+++ Exercício 09/numeros_Impares.java
@@ -0,0 +1,16 @@
+        package com.company;
+
+        import java.util.Scanner;
+
+        public class Main {
+
+            public static void main(String[] args) {
+                
+                for (int i=1; i<50; i++){
+
+                    if (i % 2 != 0){
+                        System.out.println(i);
+                    }
+                }
+            }
+        }
